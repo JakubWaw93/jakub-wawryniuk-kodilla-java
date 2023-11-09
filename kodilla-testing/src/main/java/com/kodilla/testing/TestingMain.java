f819f7e7dc0ae503a80568db066f128b4789a866
Jakub Wawryniuk <jakubwaw93@gmail.com>
2023-11-09 17:51:16 +0100

    6.2 changing  addExpectedResult & subtractExpectedResult

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,31 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
 
     public static void main(String[] args) {
+
+        //test 1
+        System.out.println("Test - pierwszy test jednostkowy.");
+
+        Calculator calc = new Calculator();
+        int a=12;
+        int b=3;
+
+        int addResult = 15;
+        if(addResult == calc.add(a, b)) {
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        int subtractResult = 9;
+        if(subtractResult == calc.subtract(a, b)) {
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error!");
+        }
 
 
     }
