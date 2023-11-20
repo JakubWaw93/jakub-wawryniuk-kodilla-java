36488df7d5e1cb261a816dcb95ce02547f947670
Jakub Wawryniuk <jakubwaw93@gmail.com>
2023-11-20 14:36:17 +0100

    7.8 Rock, paper, scissors 0.4  working

--- kodilla-rps/src/main/java/RpsTheGame.java
+++ kodilla-rps/src/main/java/RpsTheGame.java
@@ -10,7 +10,7 @@ public class RpsTheGame {
     private int pointsToWin;
     private int round =1 ;
     private boolean end = false;
-    private int playerChoice;
+    private String playerChoice;
 
 
     public void setPlayerSign(int n) {
@@ -53,43 +53,53 @@ public class RpsTheGame {
         System.out.println("Key 1 - You play rock");
         System.out.println("Key 2 - You play paper");
         System.out.println("Key 3 - You play scissors");
-        System.out.println("Key 4 - You leave the game, \nbut I will ask You if You are sure about that");
-        System.out.println("Key 5 - Our game will start from the beginning, \nyet again, i will ask if You want to leave current game");
+        System.out.println("Key x - You leave the game, \nbut I will ask You if You are sure about that");
+        System.out.println("Key n - Our game will start from the beginning, \nyet again, i will ask if You want to leave current game");
         System.out.println();
         System.out.println("Let us begin!");
     }
 
     public void makeAWeaponChoice(){
         System.out.println("Choose Your weapon");
-        playerChoice = input.nextInt();
+        playerChoice = input.next();
     }
 
     public void setUpForBattle(){
-        if (playerChoice ==1|| playerChoice ==2|| playerChoice ==3) {
-            setPlayerSign(playerChoice);
+        if (playerChoice.equals("1")|| playerChoice.equals("2")|| playerChoice.equals("3")) {
+            setPlayerSign(Integer.parseInt(playerChoice));
             setComputerSign();
             System.out.println("Round: "+round);
             System.out.println(playerName + " choose" + playerSign + "!");
             System.out.println("I choose" + computerSign + "!");
-        } else if (playerChoice==4){
-            System.out.println("Are You sure You want to leave? Y-1/N-2");
-            int confirmation = input.nextInt();
-            if (confirmation==1){
+        } else if (playerChoice.equals("x")){
+            System.out.println("Are You sure You want to leave? Y/N");
+            String confirmation = input.next();
+            if (confirmation.equals("Y")){
                 end = true;
-            }else {
+                System.out.println("Until next time.");
+            }else if(confirmation.equals("N")){
                 theGameLoop();
+            }else{
+                System.out.println("Choose correct option!");
+                setUpForBattle();
             }
-        } else if (playerChoice==5){
-            System.out.println("Are You sure You want to start over again? Y-1/N-2");
-            int confirmation2 = input.nextInt();
-            if (confirmation2==1){
+        } else if (playerChoice.equals("n")){
+            System.out.println("Are You sure You want to start over again? Y/N");
+            String confirmation2 = input.next();
+            if (confirmation2.equals("Y")){
                 round=1;
                 playerPoints=0;
                 computerPoints=0;
                 theGame();
-            } else{
+            } else if (confirmation2.equals("N")){
                 theGameLoop();
+            } else{
+                System.out.println("Choose correct option!");
+                setUpForBattle();
             }
+        }else{
+            System.out.println("Choose correct option!");
+            theGameLoop();
         }
     }
 
@@ -100,17 +110,21 @@ public class RpsTheGame {
             } else {
                 System.out.println("You are the loser!");
             }
-            System.out.println("Do You want to fight me one more time ('1'), or leave ('2')?");
-            int finalDecision = input.nextInt();
-            if (finalDecision==1) {
+            System.out.println("Do You want to fight me one more time ('Y'), or leave ('N')?");
+            String finalDecision = input.next();
+            if (finalDecision.equals("Y")) {
                 System.out.println("How many points will be needed to win?");
                 pointsToWin = input.nextInt();
                 round = 1;
                 playerPoints = 0;
                 computerPoints = 0;
                 theGameLoop();
-            } else {
+            } else if (finalDecision.equals("N")){
                 end = true;
+                System.out.println("Until next time.");
+            }else{
+                System.out.println("Choose correct option!");
+                endGame();
             }
         }
     }
@@ -146,7 +160,7 @@ public class RpsTheGame {
         theGameLoop();
 
 
-        System.out.println("Until next time.");
+
     }
 
 }
